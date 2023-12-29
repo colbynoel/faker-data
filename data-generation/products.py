bcdc9ba60bd420fb4e6ea36bc4ccf5a5d39fc758
colbynoel <colbynoel02@gmail.com>
2023-12-29 13:27:56 -0600

    inital templating for the project

---
+++ data-generation/products.py
@@ -0,0 +1,25 @@
+from faker import Faker
+import os
+from schema import (
+        Employee,
+        Purchaser,
+        ProductGroup,
+        ProductIndividual,
+        Address,
+        Cards,
+        Distributor
+        )
+fake = Faker()
+
+def generateProducts():
+    productGroups = productGroups()
+        
+def productGroups():
+    
+
+
+
+
+
+if __name__ == "__main__":
+    generateProducts()
---
+++ schema/schema.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+
+class ProductGroup(BaseModel):
+    product_name: str
+    inventory: int
+    reviews: list[str]
+    distributor: str
+    distribution_center: str
+    
+class ProductIndividual(BaseModel):
+    product_name: str
+    upc: int
+    inventory: int
+    distributor: str
+    reviews: list[str]
+    color: str
+    size: str
+
+class Purchaser(BaseModel):
+    name: str
+    purchase_history: list[ProductGroup]
+    reviews: list[str]
+    shopping_cart: list[ProductIndividual]
+    shipping_information: list[Address]
+    cards: list[Cards]
+    billing_information: list[Address]
+    wishlist: list[ProductIndividual]
+
+class Address(BaseModel):
+    street_information: str
+    zip_code: int
+    state: str
+    city: str
+    phone_number: str
+    name: str
+
+class Cards(BaseModel):
+    card_holder: str
+    card_name: str
+    card_number: int
+    security_number: int
+    expiration_date: str
+
+class Distributor(BaseModel):
+    name: str
+    products: list[ProductGroup]
+    rating: int
+    num_employees: int
+
+class Employee(BaseModel):
+    name: str
+    salary: float
+    job_title: str
+    time_employed: int
