c213b5f3344886c40859599ed03c5c1c7591a6d0
colbynoel <colbynoel02@gmail.com>
2023-12-30 12:31:48 -0600

    Generate ProductGroups

--- data-generation/products.py
+++ data-generation/products.py
@@ -1,5 +1,5 @@
 from faker import Faker
-import os
+import random
 from schema import (
         Employee,
         Purchaser,
@@ -11,15 +11,27 @@ from schema import (
         )
 fake = Faker()
 
+# class ProductGroup(BaseModel):
+#     product_name: str
+#     inventory: int
+#     reviews: list[str]
+#     distributor: str
+#     distribution_center: str
+
 def generateProducts():
     productGroups = productGroups()
         
 def productGroups():
-    
-
-
-
-
+    productGroups = []
+    file = open("assets/products.txt", "r")
+    products = file.read().split("\n")[:-1]
+    for product in products:
+        productGroups.append(ProductGroup(
+            product_name=product,
+            inventory=random.randint(1, 10_000), 
+            reviews=["Test!"],
+            distributor=fake.company(),
+            distribution_center=fake.city()))
 
 if __name__ == "__main__":
     generateProducts()
